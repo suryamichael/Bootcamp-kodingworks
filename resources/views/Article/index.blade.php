79e66a93aa118312ee71269e4dfc3ac953708ed8
suryamichael <111202214124@mhs.dinus.ac.id>
2023-06-09 10:24:26 +0700

    Complete edition, i have 2 fixed it

---
+++ resources/views/Article/index.blade.php
@@ -0,0 +1,50 @@
+<x-app-layout>
+    <x-slot name="header">
+        <div class="flex justify-between">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('Article') }}
+            </h2>
+            <a href="{{route('article.create')}}" class="border border-blue-500 hover:bg-blue-500 hover:text-white px-4 py-2 rounded-md">Create</a>
+        </div>
+        
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <table class="border-collapse table-auto w-full text-sm">
+                        <thead>
+                            <tr>
+                                <th class="border-b font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 text-left">Name</th>
+                                <th class="border-b font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 text-left">Desc</th>
+                                <th class="border-b font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 text-left">Created At</th>
+                                <th class="border-b font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 text-left">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody class="bg-white">
+                            {{-- populate our post data --}}
+                            @foreach ($article as $articles)
+                                <tr>
+                                    <td class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">{{ $articles->title }}</td>
+                                    <td class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">{{ $articles->description }}</td>
+                                    <td class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">{{ $articles->created_at }}</td>
+                                    <td class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
+                                        <a href="{{ route('article.edit', $articles->id) }}" class="border border-blue-500 hover:bg-blue-500 hover:text-white px-4 py-2 rounded-md">SHOW</a>
+                                        <a href="{{ route('article.edit', $articles->id) }}" class="border border-yellow-500 hover:bg-yellow-500 hover:text-white px-4 py-2 rounded-md">EDIT</a>
+                                        {{-- add delete button using form tag --}}
+                                        <form method="post" action="{{ route('article.destroy', $articles->id) }}" class="inline">
+                                            @csrf
+                                            @method('delete')
+                                            <button class="border border-red-500 hover:bg-red-500 hover:text-white px-4 py-2 rounded-md">DELETE</button>
+                                        </form> 
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
